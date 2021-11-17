bb19225be531558aafd764e99f3b8041e13ed654
RANJITH KUMAR D R <ranjithkumard.r@hcl.com>
2021-11-17 10:03:29 +0000

    Update DemoarpitApplication.java

--- src/main/java/com/arpit/demoarpit/DemoarpitApplication.java
+++ src/main/java/com/arpit/demoarpit/DemoarpitApplication.java
@@ -1,13 +1,18 @@
 package com.arpit.demoarpit;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoarpitApplication {
+public class DemoarpitApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoarpitApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ???????????????????????????????????????????");
 	}
 
 }
